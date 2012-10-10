f3136b9aa9bcc91f5364297906603e4727ed461f
gabfest <a_tom007@hotmail.com>
2012-10-11 00:28:24 +0700

    add comment
    add comment to class , attribute and methods

--- src/wkkp/exceedvote/model/Project.java
+++ src/wkkp/exceedvote/model/Project.java
@@ -1,8 +1,12 @@
 package wkkp.exceedvote.model;
 
+/**
+* Project in eXceedVote which contains project id and project description
+*/
 public class Project {	
-
+	/* an id of each project */
 	private int id;
+	/* a description of each project */
 	private ProjectDescription description;
 	
 	public Project(int id, String description, String name) {
@@ -10,14 +14,26 @@ public class Project {
 		this.id = id;
 	}
 	
+	/**
+	* To get the project id
+	* @return an project id
+	*/
 	public int getProjectID() {
 		return this.id;
 	}
 	
+	/**
+	* To get a project description 
+	* @return a project description
+	*/
 	public String getProjectDescription() {
 		return this.description.getDescription();
 	}
 	
+	/**
+	* To get a project name
+	* @return a project name
+	*/
 	public String getName() {
 		return this.description.getName();
 	}
