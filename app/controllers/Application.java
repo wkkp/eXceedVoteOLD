3ba8e9b63d3cf2bc89295aa00cc7c8db7b389a26
l2ollingneko <l2ollingneko@gmail.com>
2012-11-17 03:13:21 +0700

    add some logic about authentication

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,8 +7,52 @@ import views.html.*;
 
 public class Application extends Controller {
   
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
+    	/**
+    	 *    Authentication
+    	 */
+    	public static class Login {
+        
+        	public String email;
+        	public String password;
+        
+        	public String validate() {
+            		if(User.authenticate(email, password) == null) {
+                		return "Invalid user or password";
+            		}
+            		return null;
+        	}
+        
+    	}
+
+    	/**
+     	 * Login page.
+     	 */
+    	public static Result login() {
+        	return ok(
+            		login.render(form(Login.class))
+        	);
+    	}
+    
+    	/**
+      	 * Handle login form submission.
+     	 */
+    	public static Result authenticate() {
+        	Form<Login> loginForm = form(Login.class).bindFromRequest();
+        	if(loginForm.hasErrors()) {
+            		return badRequest(login.render(loginForm));
+        	} else {
+            		session("username", loginForm.get().username);
+            		return TODO; 
+        	}
+    	}
+
+    	/**
+     	 * Logout and clean the session.
+     	 */
+   	public static Result logout() {
+        	session().clear();
+        	flash("success", "You've been logged out");
+        	return TODO;
+    	}
   
 }
